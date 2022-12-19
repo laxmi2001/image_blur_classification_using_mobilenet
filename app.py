e1a012f31ca2a80b2cef97149d76895eb9fd8fba
sarithamiryala <saritha5@users.noreply.huggingface.co>
2022-12-19 10:35:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
 import streamlit as st 
+from tensorflow.keras.models import load_model
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.metrics import roc_curve,auc,classification_report,confusion_matrix
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+import cv2
+import keras
+from keras.utils import np_utils
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D,MaxPooling2D,Dense,Flatten,Dropout  
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint,ReduceLROnPlateau
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam,SGD,RMSprop,Adamax
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from sklearn.model_selection import StratifiedKFold
+from tensorflow.keras.applications import MobileNetV2
+from random import shuffle
+from tqdm import tqdm  
+import scipy
+import skimage
+from skimage.transform import resize
+import random
+import os
+from io import BytesIO
+import h5py
 
 st.title('Image Bluriness Occulded')
+
+model_file_path = "mobile_net_occ.h5"
