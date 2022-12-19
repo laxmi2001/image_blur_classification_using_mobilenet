7f8011265da1a9ebb186e3b33149ccdf107a123a
sarithamiryala <saritha5@users.noreply.huggingface.co>
2022-12-19 10:50:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -31,3 +31,36 @@ import h5py
 st.title('Image Bluriness Occulded')
 
 model_file_path = "mobile_net_occ.h5"
+
+##Blurriness Features
+
+plt. figure(figsize=(10,9))
+def variance_of_laplacian(image):
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
+def threshold(value, thresh):
+    if value > thresh:
+        return "Not Blur"
+    else:
+        return "Blur"  
+def blurr_predict(img_iter):
+  
+  def make_prediction(img_content):
+    pil_image = Image.open(img_content)
+    imgplot = plt.imshow(pil_image)
+    plt.show()
+    gray_cvimage = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2GRAY)
+    #print(gray_cvimage)
+    variance_laplacian = variance_of_laplacian(gray_cvimage)
+    #print(variance_laplacian)
+    return variance_laplacian
+
+  variance_score = make_prediction(img_iter)
+  thresh = 2000
+  variance_score = variance_score/thresh
+  predicted_label = threshold(variance_score, 1)
+  return predicted_label,variance_score
+
+image_path = "images_11.jpeg"
+predicted_label,variance_score = blurr_predict(image_path)
+print("The image is", '\033[1m' + predicted_label + '\033[0m', "with the score value of" ,round(variance_score,2))
